c1648f4d0b80015e98669c5720d74ea6227d1e9f
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-05 09:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ function makeid(l) {
 	let res = "";
 	let car_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	if (let i = 0; i < l, 1++) {
-		res = car_list.charAt(
+	if (let i = 0; i < l; i++) {
+		res += car_list.charAt(
 			math.floor(
-				math.ramdom()*car_list.length;
+				math.random()*car_list.length;
 			)
 		)
 	}
+	return res;
 }
 
 // Do not change the code below.
