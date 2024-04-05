aa94d65cc17499da2d2b371527cfd15ca2609641
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-05 09:43:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
-  // write your code here
+	let res = "";
+	let car_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	if (let i = 0; i < l, 1++) {
+		res = car_list.charAt(
+			math.floor(
+				math.ramdom()*car_list.length;
+			)
+		)
+	}
 }
 
 // Do not change the code below.
